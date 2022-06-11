49d4680c11e6acaeecb646bf85db61a8167e4501
yoav <yoavdan.m@gmail.com>
2022-06-11 18:53:31 +0300

    drodown also at mobile

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 'use strict'
 
+const mediaQuery = window.matchMedia('(max-width: 880px)')
+const elDropdown = document.querySelector('.dropdown-content')
+
+if (mediaQuery.matches) {
+  elDropdown.classList.add('hidden')
+}
+
 function toggleMenu() {
   document.body.classList.toggle('menu-toggle')
 }
@@ -9,4 +16,8 @@ function toggleModal() {
   const elOverlay = document.querySelector('.overlay')
   elModal.classList.toggle('hidden')
   elOverlay.classList.toggle('hidden')
+}
+
+function showDropdown() {
+  elDropdown.classList.toggle('hidden')
 }
